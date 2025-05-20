85dae9746a420a2ff633d50f5958a053330447f0
akashyadav2060 <yadavakash2060@gmail.com>
2025-05-20 13:30:43 +1000

    created the calculator but still working on the percentage sign

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # home_projects
 i publish 40 small projects that i finish in 40 days in here while learning. I will also publish 3 major projects as well
+Day 1: i built a simple calculator using HTMl, CSS and Javascript.
---
+++ calculator/js/main.js
@@ -0,0 +1,32 @@
+let display = document.getElementById('inputBox');
+let keys = document.querySelectorAll('button');
+let string = '';
+ const keyArray = Array.from(keys)
+ 
+ keyArray.forEach(btn => {
+    btn.addEventListener('click', e =>{
+        if (e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            display.value = string
+        }
+        else if(e.target.innerHTML =='AC'){
+            string = '';
+            display.value = string;
+        }
+        else if (e.target.innerHTML == '='){
+            string = eval(string)
+            display.value = string;
+        }
+        else
+        {
+        string += e.target.innerHTML;
+        display.value = string
+        }
+        
+    })
+ })
+
+
+
+
+ 
