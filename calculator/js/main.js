5e5c1d1e996386da97e254b051afa6f4344cee29
akashyadav2060 <yadavakash2060@gmail.com>
2025-05-21 14:45:55 +1000

    updated calculator

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -1,32 +1,30 @@
 let display = document.getElementById('inputBox');
 let keys = document.querySelectorAll('button');
 let string = '';
- const keyArray = Array.from(keys)
- 
- keyArray.forEach(btn => {
-    btn.addEventListener('click', e =>{
-        if (e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
-            display.value = string
+const keyArray = Array.from(keys);
+
+keyArray.forEach(btn => {
+    btn.addEventListener('click', e => {
+        if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            display.value = string;
         }
-        else if(e.target.innerHTML =='AC'){
+        else if (e.target.innerHTML == 'AC') {
             string = '';
             display.value = string;
         }
-        else if (e.target.innerHTML == '='){
-            string = eval(string)
+        else if (e.target.innerHTML == '=') {
+            try {
+                string = eval(string);
+                display.value = string;
+            } catch (error) {
+                display.value = 'Error';
+                string = '';
+            }
+        }
+        else {
+            string += e.target.innerHTML;
             display.value = string;
         }
-        else
-        {
-        string += e.target.innerHTML;
-        display.value = string
-        }
-        
-    })
- })
-
-
-
-
- 
+    });
+});
