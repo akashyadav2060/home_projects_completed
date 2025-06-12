4e25746c3e41a6b0b7560030a9a0a9041650f4a9
akashyadav2060 <yadavakash2060@gmail.com>
2025-06-13 01:51:03 +1000

    created all components, pages and routes

--- react_job/src/components/Card.jsx
+++ react_job/src/components/Card.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({children, bg = 'bg-gray-100'}) => {
   return (
-    <div>
-      
+    <div className={`${bg} p-6 rounded-lg shadow-md`}>
+      {children}
     </div>
   )
 }
